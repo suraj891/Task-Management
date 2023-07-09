ec146659c3a18228fa2774b7d9d97e9bdb1913d8
suraj891 <surajkr1314@gmail.com>
2023-07-09 16:19:29 +0530

    Backend

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const errorHandler = require('./middleware/errorHander');
 require("dotenv").config()
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 app.use("/task", require("./Routes/TaskRoute"))
